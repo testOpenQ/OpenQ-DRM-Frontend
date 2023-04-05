64b1f3a1769145bae3319c8df5a97fae0b9446ed
mktcode <kontakt@markus-kottlaender.de>
2023-04-05 14:47:37 +0200

    fixed content scrolling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex grow flex-col items-center bg-gray-900 p-12">
+      <main className="flex max-h-screen grow flex-col items-center overflow-auto bg-gray-900 p-12">
         <div className="max-w-7xl">{children}</div>
       </main>
     </div>
