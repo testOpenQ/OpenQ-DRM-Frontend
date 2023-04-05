90ab76e724954c027061e5266e84a1ef81ae68ea
mktcode <kontakt@markus-kottlaender.de>
2023-04-05 11:15:09 +0200

    style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex grow flex-col items-center bg-gray-900 p-12">
-        <div className="max-w-5xl">{children}</div>
+        <div className="max-w-7xl">{children}</div>
       </main>
     </div>
   );
