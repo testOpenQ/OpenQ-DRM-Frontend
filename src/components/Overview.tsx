311d8cbb96cd3d077752de1a2458e817caa6502d
mktcode <kontakt@markus-kottlaender.de>
2023-04-06 13:03:30 +0200

    preparing components for overview

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,41 +1,14 @@
-import { Campaign, addCampaign } from "~/db";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import { Campaign } from "~/db";
 import Headline from "./layout/Headline";
-import Link from "next/link";
-import { CodeBracketIcon, UsersIcon } from "@heroicons/react/24/outline";
+import Card from "./campaigns/Card";
 
 export default function Overview({ campaigns }: { campaigns: Campaign[] }) {
-  const router = useRouter();
-
-  const [campaignName, setCampaignName] = useState("");
-
-  function handleAddCampaign() {
-    setCampaignName("");
-    addCampaign({ name: campaignName })
-      .then((id) => {
-        router
-          .push(`/campaigns/${id.toString()}`)
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
-  }
-
   return (
     <>
       <Headline>Overview</Headline>
       <div className="grid grid-cols-2 gap-3">
         {campaigns.map((campaign) => (
-          <Link
-            href={`/campaigns/${campaign.id}`}
-            key={campaign.id}
-            className="group flex w-full max-w-md items-center whitespace-nowrap rounded-xl bg-indigo-900 p-6 transition-all hover:bg-indigo-800"
-          >
-            <h2 className="text-2xl font-bold">{campaign.name}</h2>
-            <CodeBracketIcon className="ml-auto mr-1 h-6 w-6 text-indigo-500" />{" "}
-            6
-            <UsersIcon className="ml-3 mr-1 h-6 w-6 text-indigo-500" /> 28
-          </Link>
+          <Card key={campaign.id} campaign={campaign} />
         ))}
       </div>
     </>
---
+++ src/components/campaigns/Card.tsx
@@ -0,0 +1,30 @@
+import { Campaign, getRepos, getUsers } from "~/db";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import DiscreetButton from "../base/DiscreetButton";
+import Link from "next/link";
+import { useLiveQuery } from "dexie-react-hooks";
+
+export default ({ campaign }: { campaign: Campaign }) => {
+  const repos = useLiveQuery(() => getRepos(campaign.id));
+  const users = useLiveQuery(() => getUsers(campaign.id));
+
+  const numberOfRepos = repos?.length ?? 0;
+  const numberOfUsers = users?.length ?? 0;
+
+  return (
+    <div className="mb-auto overflow-hidden rounded-lg bg-gray-800">
+      <div className="flex items-center justify-between bg-gray-900/50 px-3 py-2 font-bold">
+        <div className="mr-2">{campaign.name}</div>
+        <Link href={`/campaigns/${campaign.id}`}>
+          <DiscreetButton>
+            <ArrowRightIcon className="h-4 w-4" />
+          </DiscreetButton>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center space-x-6 p-3 text-center text-gray-400">
+        <div>{numberOfRepos} repos</div>
+        <div>{numberOfUsers} users</div>
+      </div>
+    </div>
+  );
+};
