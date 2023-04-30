032ec7f4236f788f777687322e83bf3811319856
mktcode <kontakt@markus-kottlaender.de>
2023-04-30 12:44:53 +0200

    added view mode toggle button in overview

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -3,14 +3,29 @@ import Headline from "./layout/Headline";
 import Card from "./campaigns/Card";
 import DiscreetButton from "./base/DiscreetButton";
 import Link from "next/link";
-import { PlusIcon } from "@heroicons/react/24/outline";
+import { EyeIcon, PlusIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 export default function Overview({ campaigns }: { campaigns: Campaign[] }) {
+  const [viewMode, setViewMode] = useState<"campaigns" | "evaluations">(
+    "campaigns"
+  );
+
   return (
     <>
       <Headline>
         Overview
         <div className="ml-auto flex">
+          <DiscreetButton
+            onClick={() =>
+              setViewMode(
+                viewMode === "campaigns" ? "evaluations" : "campaigns"
+              )
+            }
+          >
+            <EyeIcon className="mr-2 h-5 w-5" />
+            {viewMode === "campaigns" ? "Campaigns" : "Evaluations"}
+          </DiscreetButton>
           <Link href={`/campaigns/new`} className="ml-3">
             <DiscreetButton className="hover:!bg-indigo-900">
               <PlusIcon className="mr-2 h-5 w-5" />
