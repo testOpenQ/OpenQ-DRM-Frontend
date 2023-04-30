f1bb4043ca36ed24752b4b67d2bd34cc8b4b7ea4
mktcode <kontakt@markus-kottlaender.de>
2023-04-30 12:38:56 +0200

    layout

--- src/components/campaigns/Card.tsx
+++ src/components/campaigns/Card.tsx
@@ -12,7 +12,7 @@ export default function CampaignCard({ campaign }: { campaign: Campaign }) {
   const numberOfUsers = users?.length ?? 0;
 
   return (
-    <div className="mb-auto w-96 overflow-hidden rounded-lg bg-gray-800">
+    <div className="mb-auto overflow-hidden rounded-lg bg-gray-800">
       <div className="flex items-center justify-between bg-gray-900/50 px-3 py-2 font-bold">
         <div className="mr-2">{campaign.name}</div>
         <Link href={`/campaigns/${campaign.id}`}>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen">
       <Sidebar />
       <main className="flex max-h-screen grow flex-col items-center overflow-auto bg-gray-900 p-12">
-        <div className="max-w-7xl">{children}</div>
+        <div className="w-full max-w-7xl">{children}</div>
       </main>
     </div>
   );
